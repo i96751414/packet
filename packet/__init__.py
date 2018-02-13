3356c5d2902f7f37797048becce58469115d79d5
i96751414 <i96751414@gmail.com>
2018-02-13 17:38:43 +0000

    Add InspectedPacted and InspectedSafePacket

--- packet/__init__.py
+++ packet/__init__.py
@@ -4,12 +4,13 @@
 from packet.utils import *
 from packet.packet import Packet
 from packet.safepacket import SafePacket
+from packet.inspectedpacket import InspectedPacket, InspectedSafePacket
 
 __author__ = "i96751414"
 __email__ = "i96751414@gmail.com"
 __version__ = "0.0.1 (12/02/2018)"
 
-__all__ = ["Packet", "SafePacket",
+__all__ = ["Packet", "SafePacket", "InspectedPacket", "InspectedSafePacket",
            "UnknownPacket", "InvalidData", "UnknownEncryption", "NotSerializable",
            "CBC_MODE", "CTR_MODE",
            "__author__", "__email__", "__version__"]
---
+++ packet/inspectedpacket.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from packet.packet import Packet
+from packet.safepacket import SafePacket
+from packet.utils import NotSerializable, InvalidData
+
+__allowed_types = ["dict",
+                   "list", "tuple",
+                   "str", "unicode",
+                   "int", "long", "float",
+                   "bool",
+                   "NoneType"]
+
+
+def _is_instance_of_class(var):
+    # TODO - improve this check: instances of classes with a __call__() method are also callable
+    return not callable(var) and hasattr(var, "__dict__")
+
+
+def _generate_dict(obj):
+    _dict = {}
+    for k, v in obj.__dict__.items():
+        t = _type_string(v)
+        if t in __allowed_types:
+            _dict[k] = v
+        elif _is_instance_of_class(v):
+            _dict[k] = _generate_dict(v)
+        else:
+            raise NotSerializable
+    return _dict
+
+
+def _type_string(var):
+    return var.__class__.__name__
+
+
+def __check_dict(obj, data):
+    if not isinstance(data, dict) or set(obj.__dict__) != set(data):
+        raise Exception
+    for k, v in obj.__dict__.items():
+        t = _type_string(v)
+        if t in __allowed_types:
+            if t != _type_string(data[k]):
+                raise Exception
+        elif _is_instance_of_class(v):
+            if _type_string(data[k]) != "dict":
+                raise Exception
+            __check_dict(v, data[k])
+        else:
+            raise Exception
+
+
+def __update_dict(obj, data):
+    for k, v in obj.__dict__.items():
+        if _type_string(v) in __allowed_types:
+            obj.__dict__[k] = data[k]
+        else:
+            _update_dict(obj.__dict__[k], data[k])
+
+
+def _update_dict(obj, data):
+    try:
+        __check_dict(obj, data)
+    except Exception:
+        raise InvalidData
+    __update_dict(obj, data)
+
+
+class InspectedPacket(Packet):
+    """
+    Inspected packet class
+    """
+
+
+InspectedPacket._generate_dict = _generate_dict
+InspectedPacket._update_dict = _update_dict
+
+
+class InspectedSafePacket(InspectedPacket, SafePacket):
+    """
+    Inspected and safe packet class
+    """
