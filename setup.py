09666b2588da9d757c584bc4e8264f39aeff73ff
i96751414 <i96751414@gmail.com>
2018-03-03 19:42:23 +0000

    Use readme as long_description

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@
 from setuptools import setup
 from packet import __author__, __email__, __version__
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="packet",
     version=__version__,
     description="A simple object serializer which can be used in socket communications",
+    long_description=long_description,
     license="GPLv3",
     author=__author__,
     author_email=__email__,
